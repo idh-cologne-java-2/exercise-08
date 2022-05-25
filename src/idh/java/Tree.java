1e49d8b5effff7ba2fca6fd343163747030d9ae6
sophiejorns <sophiejorns@sophies-macbook-air.fritz.box>
2022-05-25 17:56:23 +0200

    Aufgabe 2 fertig

--- src/idh/java/Tree.java
+++ src/idh/java/Tree.java
@@ -42,14 +42,15 @@ public class Tree<T>  {
 	
 	public static void main(String[] args) {
 		
-		Tree<String> ebike = new Tree<String>("e-bike");
-		Tree<String> tandem = new Tree<String>("tandem");
-		Tree<String> bike = new Tree<String>("bike");
-		Tree<String> buggy = new Tree<String>("buggy");
 		Tree<String> wheeled_vehicle = new Tree<String>("wheeled vehicle");
-
-		wheeled_vehicle.children().add(bike);
+		Tree<String> bike = new Tree<String>("  bike");
+		Tree<String> ebike = new Tree<String>("    e-bike");
+		Tree<String> tandem = new Tree<String>("    tandem");
+		Tree<String> buggy = new Tree<String>("  buggy");
+		
 		wheeled_vehicle.children().add(buggy);
+		wheeled_vehicle.children().add(bike);
+		
 		bike.children().add(tandem);
 		bike.children().add(ebike);
 		

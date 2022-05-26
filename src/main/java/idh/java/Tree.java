cdb6cf2081048ae76da930d9f799ba9b2c36126b
JRadermacher <jana.radermacher7@gmx.de>
2022-05-26 12:11:42 +0200

    Hausaufgabe 08 erledigt

--- src/main/java/idh/java/Tree.java
+++ src/main/java/idh/java/Tree.java
@@ -11,12 +11,12 @@ public class Tree<T>  {
 	
 	public Tree(T value) {
 		this.value = value;
-		this.children = new HashSet<Tree<T>>();
+		this.children = new HashSet<>();
 	}
 	
 	public Tree(T value, Collection<Tree<T>> children) {
 		this.value = value;
-		this.children = new HashSet<Tree<T>>(children);
+		this.children = new HashSet<>(children);
 	}
 		
 	public T get() {
@@ -32,28 +32,38 @@ public class Tree<T>  {
 	}
 	
 	
-	public void dfs() {
-		System.out.println(this.value);
+	public void dfs(int depth) {
+
+		/*StringBuilder str = new StringBuilder();
+
+		for(int i = 0; i < depth; i++){
+			str.append("  ");
+		}
+		 */
+
+		System.out.println(" ".repeat(depth * 2) + this.value);
+
 		for (Tree<T> child : children) {
-			child.dfs();
+			child.dfs(depth + 1);
 		}
 	}
 	
 	
 	public static void main(String[] args) {
 		
-		Tree<String> ebike = new Tree<String>("e-bike");
-		Tree<String> tandem = new Tree<String>("tandem");
-		Tree<String> bike = new Tree<String>("bike");
-		Tree<String> buggy = new Tree<String>("buggy");
-		Tree<String> wheeled_vehicle = new Tree<String>("wheeled vehicle");
+		Tree<String> ebike = new Tree<>("e-bike");
+		Tree<String> tandem = new Tree<>("tandem");
+		Tree<String> bike = new Tree<>("bike");
+		Tree<String> buggy = new Tree<>("buggy");
+
+		Tree<String> wheeled_vehicle = new Tree<>("wheeled vehicle");
 
 		wheeled_vehicle.children().add(bike);
 		wheeled_vehicle.children().add(buggy);
 		bike.children().add(tandem);
 		bike.children().add(ebike);
 		
-		wheeled_vehicle.dfs();
+		wheeled_vehicle.dfs(0);
 	}
 
 }

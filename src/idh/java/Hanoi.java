763cebfb567b542f86d2c0fd71ddcfc06d0956f1
sophiejorns <sophiejorns@sophies-macbook-air.fritz.box>
2022-05-26 14:22:21 +0200

    Türme von Hanoi nun rekursiv

--- src/idh/java/Hanoi.java
+++ src/idh/java/Hanoi.java
@@ -108,6 +108,16 @@ public class Hanoi {
 	
 	private void movePieces(int numberOfPieces, char from, char to, char util) {
 		// TODO: Implement me!
+		
+		if (numberOfPieces == 0) {
+			return;
+			
+		} else {
+			movePieces(numberOfPieces - 1, from, util, to);
+			movePiece(from, to);
+			System.out.println("Move Piece " + numberOfPieces + " from " + from + " to " + to);
+			movePieces(numberOfPieces - 1, util, to, from);
+		}
 	}
 	
 	

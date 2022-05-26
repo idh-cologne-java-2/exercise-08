57e90550b06e932c5596c39d9bb61564db766ee2
JRadermacher <jana.radermacher7@gmx.de>
2022-05-26 12:32:47 +0200

    Hausaufgabe 08 done

--- src/main/java/idh/java/Tree.java
+++ src/main/java/idh/java/Tree.java
@@ -4,65 +4,66 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Tree<T>  {
+public class Tree<T> {
 
-	T value;
-	Set<Tree<T>> children;
-	
-	public Tree(T value) {
-		this.value = value;
-		this.children = new HashSet<>();
-	}
-	public Tree(T value, Collection<Tree<T>> children) {
-		this.value = value;
-		this.children = new HashSet<>(children);
-	}
-		
-	public T get() {
-		return value;
-	}
+    T value;
+    Set<Tree<T>> children;
 
-	public void set(T value) {
-		this.value = value;
-	}
+    public Tree(T value) {
+        this.value = value;
+        this.children = new HashSet<>();
+    }
 
-	public Set<Tree<T>> children() {
-		return children;
-	}
-	
-	
-	public void dfs(int depth) {
+    public Tree(T value, Collection<Tree<T>> children) {
+        this.value = value;
+        this.children = new HashSet<>(children);
+    }
 
-		/*StringBuilder str = new StringBuilder();
+    public T get() {
+        return value;
+    }
 
-		for(int i = 0; i < depth; i++){
-			str.append("  ");
-		}
-		 */
+    public void set(T value) {
+        this.value = value;
+    }
 
-		System.out.println(" ".repeat(depth * 2) + this.value);
+    public Set<Tree<T>> children() {
+        return children;
+    }
 
-		for (Tree<T> child : children) {
-			child.dfs(depth + 1);
-		}
-	}
-	
-	
-	public static void main(String[] args) {
-		
-		Tree<String> ebike = new Tree<>("e-bike");
-		Tree<String> tandem = new Tree<>("tandem");
-		Tree<String> bike = new Tree<>("bike");
-		Tree<String> buggy = new Tree<>("buggy");
+    public void dfs(int depth) {
+        StringBuilder str = new StringBuilder();
 
-		Tree<String> wheeled_vehicle = new Tree<>("wheeled vehicle");
+        for (int i = 0; i < depth; i++) {
+           str.append("  ");
+        }
+        str.append(this.value);
+        System.out.println(str);
 
-		wheeled_vehicle.children().add(bike);
-		wheeled_vehicle.children().add(buggy);
-		bike.children().add(tandem);
-		bike.children().add(ebike);
-		
-		wheeled_vehicle.dfs(0);
-	}
+        //Variante mit String.repeat
+        //System.out.println(" ".repeat(depth * 2) + this.value);
+
+        for (Tree<T> child : children) {
+            child.dfs(depth + 1);
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        Tree<String> ebike = new Tree<>("e-bike");
+        Tree<String> tandem = new Tree<>("tandem");
+        Tree<String> bike = new Tree<>("bike");
+        Tree<String> buggy = new Tree<>("buggy");
+
+        Tree<String> wheeled_vehicle = new Tree<>("wheeled vehicle");
+
+        wheeled_vehicle.children().add(bike);
+        wheeled_vehicle.children().add(buggy);
+        bike.children().add(tandem);
+        bike.children().add(ebike);
+
+        wheeled_vehicle.dfs(0);
+    }
 
 }

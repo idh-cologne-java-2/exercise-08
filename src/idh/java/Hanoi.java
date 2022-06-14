bac01739726b597837ebc186d7b9d4eaf18917de
Alessandro M <sando1199@web.de>
2022-06-14 13:49:18 +0200

    Aufgaben fertig

--- src/idh/java/Hanoi.java
+++ src/idh/java/Hanoi.java
@@ -107,7 +107,14 @@ public class Hanoi {
 	}
 	
 	private void movePieces(int numberOfPieces, char from, char to, char util) {
-		// TODO: Implement me!
+		if(numberOfPieces > 0) {
+			movePieces(numberOfPieces-1, from, util, to);
+			movePiece(from, to);
+			movePieces(numberOfPieces-1, util, to, from);
+		}
+		else {
+			return;
+		}
 	}
 	
 	
